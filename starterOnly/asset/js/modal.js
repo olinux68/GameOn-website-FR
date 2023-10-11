01c9c1ece72978de506d4df4eb427f7e885f34e1
olinux68 <34310840+olinux68@users.noreply.github.com>
2023-10-11 11:37:35 +0200

    mise a jours des test du modal
    problème de destination du message de remercîment

--- starterOnly/asset/js/modal.js
+++ starterOnly/asset/js/modal.js
@@ -1,3 +1,6 @@
+//@ts-nocheck
+
+// Fonction pour gérer la navigation responsive
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,23 +10,173 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// Récupération des éléments du DOM
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const modalBtnClose = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const locationTournament = document.getElementsByName("location");
+const condition = document.getElementById("checkbox1");
 
+// Éléments pour les messages de validation
+const firstText = document.getElementById("firstText");
+const lastText = document.getElementById("lastText");
+const emailText = document.getElementById("emailText");
+const birthdateText = document.getElementById("birthdateText");
+const quantityText = document.getElementById("quantityText");
+const locationText = document.getElementById("locationText");
+const conditionText = document.getElementById("conditionText");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// Expressions régulières pour la validation
+let regExTypeText = new RegExp('^([A-Za-z]{2,20})?([-]{0,1})?([A-Za-z]{2,20})$');
+let regExTypeEmail = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$');
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+// Fonction générique pour valider les champs texte et email
+function generiqueValidate(input, regEx, msg, label, border) {
+  let testValid = regEx.test(input.value);
+  if (testValid) {
+    label.innerHTML = "Champs Valide";
+    label.classList.remove('text-danger');
+    label.classList.add('text-succes');
+    border.classList.remove('border-danger');
+    border.classList.add('border-succes');
+    return true;
+  } else {
+    label.innerHTML = msg;
+    label.classList.remove('text-succes');
+    label.classList.add('text-danger');
+    border.classList.remove('border-succes');
+    border.classList.add('border-danger');
+    return false;
+  }
 }
 
-modalBtnClose.addEventListener("click", function () {          
-    modalbg.style.display = "none";                            
+// Validation pour le prénom
+first.addEventListener('change', function () {
+  generiqueValidate(this, regExTypeText, "Veuillez rentrer deux caractères minimum", firstText, this);
 });
 
+// Validation pour le nom
+last.addEventListener('change', function () {
+  generiqueValidate(this, regExTypeText, "Veuillez rentrer deux caractères minimum", lastText, this);
+});
 
+// Validation pour l'email
+email.addEventListener('change', function () {
+  generiqueValidate(this, regExTypeEmail, "Veuillez rentrer un adresse email valide", emailText, this);
+});
+
+// Validation pour la date de naissance
+birthdate.addEventListener('change', function () {
+  validBirthdate(this);
+});
+
+// Validation pour le nombre de tournois
+quantity.addEventListener('change', function () {
+  validQuantity(this);
+});
+
+// Validation pour la localisation
+locationTournament.forEach((checkedBoxInput) => checkedBoxInput.addEventListener('change', function () {
+  validLocationTournament();
+}));
+
+// Validation pour les conditions
+condition.addEventListener('change', function () {
+  validCondition(this);
+});
+
+// Fonction de validation pour la date de naissance
+function validBirthdate(input) {
+  if (!input.value) {
+    birthdateText.innerHTML = "Veuillez entrer une date de naissance valide";
+    birthdateText.classList.add('text-danger');
+    return false;
+  } else {
+    birthdateText.innerHTML = "Champs Valide";
+    birthdateText.classList.remove('text-danger');
+    birthdateText.classList.add('text-succes');
+    return true;
+  }
+}
+
+// Fonction de validation pour le nombre de tournois
+function validQuantity(input) {
+  if (input.value === "0" || input.value < 0) {
+    quantityText.innerHTML = "Merci d'indiquer le nombre de tournois";
+    quantityText.classList.add('text-danger');
+    return false;
+  } else {
+    quantityText.innerHTML = "Champs Valide";
+    quantityText.classList.remove('text-danger');
+    quantityText.classList.add('text-succes');
+    return true;
+  }
+}
+
+// Fonction de validation pour la localisation
+function validLocationTournament() {
+  for (let i = 0; i < locationTournament.length; i++) {
+    if (locationTournament[i].checked) {
+      locationText.innerHTML = "Champs Valide";
+      locationText.classList.remove('text-danger');
+      locationText.classList.add('text-succes');
+      return true;
+    }
+  }
+  locationText.innerHTML = "Merci de cocher une ville";
+  locationText.classList.add('text-danger');
+  return false;
+}
+
+// Fonction de validation pour les conditions
+function validCondition(input) {
+  if (!input.checked) {
+    conditionText.innerHTML = "Merci d'accepter les conditions d'utilisations";
+    conditionText.classList.add('text-danger');
+    return false;
+  } else {
+    conditionText.innerHTML = "Champs Valide";
+    conditionText.classList.remove('text-danger');
+    conditionText.classList.add('text-succes');
+    return true;
+  }
+}
+
+// Événement pour ouvrir le formulaire modal
+modalBtn.forEach((btn) => btn.addEventListener("click", function () {
+  modalbg.style.display = "block";
+}));
+
+// Événement pour fermer le formulaire modal
+document.querySelector(".close").addEventListener("click", function () {
+  modalbg.style.display = "none";
+});
+
+// Événement pour la validation finale du formulaire
+document.getElementById("btnSubmit").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (
+    generiqueValidate(first, regExTypeText, "Prénom invalide", firstText, first) &&
+    generiqueValidate(last, regExTypeText, "Nom invalide", lastText, last) &&
+    generiqueValidate(email, regExTypeEmail, "Email invalide", emailText, email) &&
+    validBirthdate(birthdate) &&
+    validQuantity(quantity) &&
+    validLocationTournament() &&
+    validCondition(condition)
+  ) {
+    // Cacher le formulaire dans le modal
+    formData.forEach((el) => el.style.display = "none");
+
+    // Afficher le message de remerciement dans le modal
+    let thankYouMessage = document.createElement("div");
+    thankYouMessage.innerHTML = "<h2>Merci pour votre inscription !</h2>";
+    modalbg.appendChild(thankYouMessage);
+  } else {
+    alert("Merci de remplir correctement votre inscription.");
+  }
+});
